f5eeac3b5b093d630429166fe2a7805c77faafab
Joseph Roque <contact@josephroque.ca>
2015-10-29 11:25:13 -0400

    Added constraints to user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,28 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, presence: true, uniqueness: true, :length => {
-    :minimum => 2,
-    :maximum => 50}
+  # Verifying valid username
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { in: 2..50 },
+    format: { with: /\A[a-z0-9-]+\z/i, message: 'only numbers, letters and hyphens allowed' }
+
+  # Verifying valid email
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    length: { in: 12..50},
+    format: { with: /\A([^@\s]+)@uottawa[.]ca\z/i, message: 'must be an @uottawa.ca email' }
+
+  # Verifying minimum password length
+  validates :password,
+    presence: true,
+    length: { minimum: 6, too_short: 'passwords must be minimum %{count} characters'}
+
+  # Verifying maximum length of about section
+  validates :about,
+    presence: true,
+    length: { maximum: 500, too_long: 'maximum %{count} characters' }
 
 end
