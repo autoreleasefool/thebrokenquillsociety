c47a947d89be989d072d7260fa73c734e2d3b5c6
Joseph Roque <contact@josephroque.ca>
2015-10-28 20:12:22 -0400

    Finished user sign up page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( works.css, users.css )
+Rails.application.config.assets.precompile += %w( works.css , users.css )
