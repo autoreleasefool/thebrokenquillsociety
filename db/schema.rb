e497fb13fb283e305e86ab9568c1f59e624e5e9e
Joseph Roque <contact@josephroque.ca>
2015-10-31 13:12:52 -0400

    Added admin attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029192324) do
+ActiveRecord::Schema.define(version: 20151031171049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20151029192324) do
     t.text     "about"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
