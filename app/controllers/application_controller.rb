a4925e2bdd1f301fdb0d24892828d98156ee6a8a
Joseph Roque <contact@josephroque.ca>
2015-11-03 23:31:06 -0500

    Added formatting to comments on works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Gets the current logged in user or nil
+  # Helper method to get the current logged in user or nil
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
