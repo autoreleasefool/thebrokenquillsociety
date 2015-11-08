59362898fe7595a0ad1d31c02441508545279c43
Joseph Roque <contact@josephroque.ca>
2015-11-07 22:49:30 -0500

    Linked to new announcement page

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,6 +3,9 @@ class Announcement < ActiveRecord::Base
   # Associations
   belongs_to :user
 
+  # Save emails in lowercase
+  before_save { prefix_link }
+
   # Verifying title exists
   validates :title,
     presence: { message: 'Your announcement must have a title.' },
@@ -12,5 +15,13 @@ class Announcement < ActiveRecord::Base
   validates :body,
     presence: { message: 'You cannot submit a blank announcement.' },
     length: { maximum: 255, too_long: 'Body can be a maximum %{count} characters.' }
+
+  def prefix_link
+    if self.link && self.link.length > 0
+      unless self.link.starts_with?('http')
+        self.link = 'http://' + self.link
+      end
+    end
+  end
 
 end
