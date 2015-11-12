9d48e050f889090f0e5fe17b5da4e0ea27e5192d
Joseph Roque <contact@josephroque.ca>
2015-11-12 00:23:45 -0500

    Fixed confirmation routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+
+  # Enabling devise
+  devise_for :users
+
   resources :works do
     resources :comments
   end
@@ -6,11 +12,6 @@ Rails.application.routes.draw do
   resources :users
   resources :announcements
   resources :abouts
-
-  root 'application#index'
-
-  # Enabling devise
-  devise_for :users
 
   # Application routes
   get '/search' => 'application#search'
@@ -21,7 +22,7 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
 
   # Routes to login to / logout of a user's account
-  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#new', :as => :new_session
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
@@ -30,5 +31,12 @@ Rails.application.routes.draw do
   get '/faves' => 'users#faves'
   post '/like' => 'users#add_favourite'
   post '/unlike' => 'users#remove_favourite'
+
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'sign-in' => 'devise/sessions#new', :as => :new_user_session
+    post 'sign-in' => 'devise/sessions#create', :as => :user_session
+    delete 'sign-out' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
 end
