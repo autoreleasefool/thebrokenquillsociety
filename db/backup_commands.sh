38b9af1a7f38cefe922ec9cfc9d921c3fdbea278
Joseph Roque <contact@josephroque.ca>
2015-11-28 16:22:56 -0500

    Recorded commands to backup database
    #7, need to make automatic next

---
+++ db/backup_commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#################################################
+#                                               #
+#    Use the following two commands to make a   #
+#      backup of the production database.       #
+#  The first will set a variable to the current #
+#  date to use as the file name and the second  #
+#     will export the database to the file.     #
+#                                               #
+#################################################
+
+export current_date=`date +"%Y%m%d_%H%M%N"`
+pg_dump -F c -v -U rails -h localhost uo-cwc_production -f ~rails/uo-cwc/backup/${current_date}.sql
