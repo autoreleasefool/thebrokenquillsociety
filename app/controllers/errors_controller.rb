b08b94eb7929c22d63502a7e57e94c21f385cbcb
Joseph Roque <contact@josephroque.ca>
2016-01-07 15:24:12 -0500

    Testing new error pages in development

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,13 @@
 class ErrorsController < ApplicationController
 
+  # Displays the error page pertinent to the error code
   def show
     render status_code.to_s, :status => status_code
   end
 
 protected
 
+  # Gets the error code, or returns 500 for internal error if no code is available
   def status_code
     params[:code] || 500
   end
