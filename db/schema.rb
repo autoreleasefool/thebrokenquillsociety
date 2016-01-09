854784aabae59d5c5886b8bde0f3c51c9e50723b
Joseph Roque <contact@josephroque.ca>
2016-01-08 20:52:41 -0800

    Added column for indicating unread notifications
    #1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103233922) do
+ActiveRecord::Schema.define(version: 20160109044949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20160103233922) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "unread"
   end
 
   add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
