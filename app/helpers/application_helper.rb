23e7804f4399be90ed03e97f8bf3482d917047b6
Joseph Roque <contact@josephroque.ca>
2016-01-13 21:28:54 -0800

    Changed preview on index to 3 lines or 255 characters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  # Returns the index of the 3rd newline of a string (the most lines to show of a preview).
+  # Or, if there are less than 3 newlines or the 3rd newline is greater than 255 characters
+  # into the string, then return the first 255 characters.
+  def length_of_preview(body)
+    newline_count = 0
+    last_newline = -1
+    until newline_count == 3 || last_newline == nil
+      last_newline = body.index("\n", last_newline + 1)
+      newline_count += 1
+    end
+
+    if last_newline == nil || last_newline > 255
+      return 255
+    else
+      return last_newline
+    end
+  end
+
 end
