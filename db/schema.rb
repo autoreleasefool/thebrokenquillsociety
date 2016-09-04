b34e505a29c67690b74ca28aafbea36b661096f4
Joseph Roque <contact@josephroque.ca>
2016-09-04 12:34:26 -0400

    Update migrations for new versions

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308042759) do
+ActiveRecord::Schema.define(version: 20160904163110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +29,8 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.boolean  "options_enabled"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_admin_options_on_user_id", using: :btree
   end
-
-  add_index "admin_options", ["user_id"], name: "index_admin_options_on_user_id", using: :btree
 
   create_table "announcements", force: :cascade do |t|
     t.text     "title"
@@ -41,9 +39,8 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "link"
+    t.index ["user_id"], name: "index_announcements_on_user_id", using: :btree
   end
-
-  add_index "announcements", ["user_id"], name: "index_announcements_on_user_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -51,20 +48,18 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.integer  "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+    t.index ["work_id"], name: "index_comments_on_work_id", using: :btree
   end
-
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-  add_index "comments", ["work_id"], name: "index_comments_on_work_id", using: :btree
 
   create_table "favourites", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favourites_on_user_id", using: :btree
+    t.index ["work_id"], name: "index_favourites_on_work_id", using: :btree
   end
-
-  add_index "favourites", ["user_id"], name: "index_favourites_on_user_id", using: :btree
-  add_index "favourites", ["work_id"], name: "index_favourites_on_work_id", using: :btree
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -72,22 +67,20 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.string   "sluggable_type", limit: 50
     t.string   "scope"
     t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
   end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "histories", force: :cascade do |t|
     t.integer  "work_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_histories_on_user_id", using: :btree
+    t.index ["work_id"], name: "index_histories_on_work_id", using: :btree
   end
-
-  add_index "histories", ["user_id"], name: "index_histories_on_user_id", using: :btree
-  add_index "histories", ["work_id"], name: "index_histories_on_work_id", using: :btree
 
   create_table "notifications", force: :cascade do |t|
     t.text     "body"
@@ -98,30 +91,34 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.datetime "updated_at", null: false
     t.boolean  "unread"
     t.integer  "notifier"
+    t.index ["notifier"], name: "index_notifications_on_notifier", using: :btree
+    t.index ["user_id"], name: "index_notifications_on_user_id", using: :btree
   end
-
-  add_index "notifications", ["notifier"], name: "index_notifications_on_notifier", using: :btree
-  add_index "notifications", ["user_id"], name: "index_notifications_on_user_id", using: :btree
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
-    t.integer  "taggable_id"
     t.string   "taggable_type"
-    t.integer  "tagger_id"
+    t.integer  "taggable_id"
     t.string   "tagger_type"
+    t.integer  "tagger_id"
     t.string   "context",       limit: 128
     t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context", using: :btree
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy", using: :btree
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id", using: :btree
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type", using: :btree
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type", using: :btree
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id", using: :btree
   end
-
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
-
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
@@ -136,10 +133,9 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.text     "admin_description"
     t.string   "slug"
     t.integer  "unread_notifications"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
   create_table "works", force: :cascade do |t|
     t.text     "title"
@@ -149,9 +145,8 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.index ["user_id"], name: "index_works_on_user_id", using: :btree
   end
-
-  add_index "works", ["user_id"], name: "index_works_on_user_id", using: :btree
 
   add_foreign_key "admin_options", "users"
   add_foreign_key "announcements", "users"
