98fe9dc08d2f4718e75df965477ea859b86de118
Joseph Roque <contact@josephroque.ca>
2016-09-08 18:03:54 -0400

    Reset password for user after they click link

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,4 +105,9 @@ extend FriendlyId
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 end
