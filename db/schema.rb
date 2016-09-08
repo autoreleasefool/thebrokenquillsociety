482956525c8b6d399a6f6529e67e8fa3bedaabd4
Joseph Roque <contact@josephroque.ca>
2016-09-07 20:40:50 -0400

    Add columns to database for password reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308042759) do
+ActiveRecord::Schema.define(version: 20160908002914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,8 @@ ActiveRecord::Schema.define(version: 20160308042759) do
     t.text     "admin_description"
     t.string   "slug"
     t.integer  "unread_notifications"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
