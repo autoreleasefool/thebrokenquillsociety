acdbf5f5747fbc56d3e8c83e89e785abd58ff4ae
Joseph Roque <contact@josephroque.ca>
2016-09-10 20:34:28 -0400

    Add host configuration to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    host: UntitledCWC::Application::WEBSITE_DOMAIN
+  }
   config.action_mailer.default_options = {
     from: 'no-reply@' + UntitledCWC::Application::WEBSITE_DOMAIN
   }
