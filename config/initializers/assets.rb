fec1d7cc730ca1aaff459ce9492cd3e59a19c9ee
Joseph Roque <contact@josephroque.ca>
2016-09-14 19:49:52 -0400

    Add novels stylesheet to assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( js.cookie.js works.css users.css sessions.css comments.css announcements.css abouts.css errors.css notifications.css password_resets.css mobile.css)
+Rails.application.config.assets.precompile += %w( js.cookie.js works.css users.css sessions.css comments.css announcements.css abouts.css errors.css notifications.css password_resets.css mobile.css novels.css)
